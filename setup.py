337dc1d29dd7021a6aff124966c2bda575f3387d
Bernhard Miklautz <bmiklautz+jobsta@shacknet.at>
2023-12-13 11:59:39 +0100

    Pump version to 2.7.3

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(
-        version="2.7.2",
+        # based on fpdf2 version 2.7.7
+        # use same major and minor version number as fpdf2 and increase patch number for each following
+        # reportbro-fpdf2 release
+        version="2.7.3",
     )
