1fe05937f0b0b6c24b4684f98fd75de38ee36822
Alex Hartmann <23240822+alhman@users.noreply.github.com>
2024-11-05 20:12:41 +0100

    Increase version to 2.7.6

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
         # based on fpdf2 version 2.7.7
         # use same major and minor version number as fpdf2 and increase patch number for each following
         # reportbro-fpdf2 release
-        version="2.7.5",
+        version="2.7.6",
     )
